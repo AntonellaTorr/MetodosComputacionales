ff685d30f831fd3c49d6a664b9d6b6d0060cd517
AntonellaTorr <bantonellatorres@gmail.com>
2021-09-21 13:17:04 -0300

    Metodo de trapecios y simpson con datos tabulados

--- Unidad1/Metodos.java
+++ Unidad1/Metodos.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 
 public class Metodos{
     public static void main(String[] args) {
-        
-      metodoTrapecios();
+        metodoTrapecios();
+    
     }
     public static void MetodoBiseccion() {
         Scanner leer= new Scanner(System.in);
@@ -38,16 +38,13 @@ public class Metodos{
 
     public static double f(double x) {
         double r;
-        //r=Math.pow(x, 3)-2*x-1;
-        // r= 2*Math.sin(x+1);
     
-        r=Math.pow(Math.E, x);
+        r=Math.pow(x,2);
         return r;
         
     }
     public static double fDerivada (double x){
-       // double r= 2*Math.cos(x+1);
-       double r=-160*Math.exp(-2*x)-10*Math.exp(-0.5*x);
+       double r=3*Math.pow(x,2)+4*x+10;
         return r;
     }
     public static void metodoNewthonRaphson() {
@@ -68,7 +65,7 @@ public class Metodos{
            
             i++;
            
-        }while (Math.abs(f(r))>error && i<25);
+        }while (Math.abs(f(r))>=error && i<25);
         System.out.println ("Valor de la raiz aproximada " +r);
        
 
@@ -83,13 +80,24 @@ public class Metodos{
         int n=leer.nextInt();
         double h=(b-a)/n;
         System.out.println ("El valor de h es "+h);
-        double fx0= f(a);
-        double fxn=f(b);
-        double sumatoria=0,xi=a+h;
+        double f[]= new double [n+1];
+
+        System.out.println ("Desea trabajar con valores tabulados?true/false");
+        boolean res= leer.nextBoolean();
+        
+        if (res==true){
+            f= llenarArregloManualmente(f);
+        }
+        else{
+            f=llenarArregloConLaFuncion(f, h, a);
+        }
+        
+        double fx0= f[0];
+        double fxn=f[n];
+        double sumatoria=0;
         int i=1;
         while (i<=n-1){
-            sumatoria+=f(xi);
-            xi+=h;
+            sumatoria+=f[i];
             i++;
         }
         double resultado=h/2*(fx0+fxn+ 2*sumatoria);
@@ -106,29 +114,58 @@ public class Metodos{
         int n=leer.nextInt();
         double h=(b-a)/n;
         System.out.println ("El valor de h es "+h);
-        double fx0= f(a);
-        double fxn=f(b);
-        double sumatoriaImpar=0,sumatoriaPar=0,xi=a+h,xj=a+h+h;
+        double f[]= new double [n+1];
+
+        System.out.println ("Desea trabajar con valores tabulados?true/false");
+        boolean res= leer.nextBoolean();
+        
+        if (res==true){
+            f= llenarArregloManualmente(f);
+        }
+        else{
+            f=llenarArregloConLaFuncion(f, h, a);
+        }
+        
+        double fx0= f[0];
+        double fxn=f[n];
+        double sumatoriaImpar=0,sumatoriaPar=0;
         int i=1;
         while (i<=n-1){
             if (i%2!=0){              
-                sumatoriaImpar+=f(xi);
+                sumatoriaImpar+=f[i];
             }
-            xi+=h;
+            
             i++;
         }
         int j=2;
         while (j<=n-2){
             if (j%2==0){
-                sumatoriaPar+=f(xj);
+                sumatoriaPar+=f[j];
             }
-            xj+=h;
+           
             j++;
         }
         double resultado= h/3*(fx0+fxn+4*sumatoriaImpar+2*sumatoriaPar);
 
         System.out.println ("Resultado aproximado de la integral "+resultado);
         
+        
+    }
+    public static double [] llenarArregloManualmente(double [] array){
+        Scanner leer= new Scanner(System.in);
+        for (int i=0; i<array.length;i++){
+            System.out.println ("Ingrese el valor para el x"+i);
+            array[i]=leer.nextDouble();
+            System.out.println (array[i]);
+        }
+        return array;
+    }
+    public static double [] llenarArregloConLaFuncion(double [] array, double h, double a){
+        for (int i=0; i<array.length;i++){
+            array[i]=f(a);
+            a+=h;
+        }
+        return array;
     }
 }
 
